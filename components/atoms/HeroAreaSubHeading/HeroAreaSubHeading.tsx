062dedc0a137c91cf7cf234738844f70537063d0
Ashraf <ashrafali.gcs@gmail.com>
2024-11-20 09:01:02 +0500

    Hero area icon issue

--- components/atoms/HeroAreaSubHeading/HeroAreaSubHeading.tsx
+++ components/atoms/HeroAreaSubHeading/HeroAreaSubHeading.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-const HeroAreaSubHeading = ( {path}: {path: string} ) => {
+const HeroAreaSubHeading = ({ path }: { path: string }) => {
   return (
-        <div className=' text-base ' >
-            <a href='/'> Home </a> &#62;&#62; <span>{path}</span>
-        </div>
-  )
-}
+    <div className=" text-base flex mt-2">
+      <a href="/"> Home </a>{" "}
+      <span className="mt-1 px-1">
+        {" "}
+        <FaAngleDoubleRight color="#ffffff" size={16} />
+      </span>{" "}
+      <span>{path}</span>
+    </div>
+  );
+};
 
-export default HeroAreaSubHeading
+export default HeroAreaSubHeading;
