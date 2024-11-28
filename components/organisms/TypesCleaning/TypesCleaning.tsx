a13141ecea90d74b7592fab1f0003df1c8143435
Ashraf <ashrafali.gcs@gmail.com>
2024-11-28 22:50:58 +0500

    Fix types of cleaning

--- components/organisms/TypesCleaning/TypesCleaning.tsx
+++ components/organisms/TypesCleaning/TypesCleaning.tsx
@@ -1,9 +1,11 @@
+
 "use client";
-import Heading from "@/components/atoms/Heading/heading";
 import React from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
+import Image from "next/image";
 
+import Heading48 from "@/components/atoms/Heading48/Heading48";
 // cart Icon
 import cartIcon1 from "@/assets/images/cleaningServiceImages/service-cat-icon-1.png";
 import cartIcon2 from "@/assets/images/cleaningServiceImages/service-cat-icon-2.png";
@@ -14,13 +16,12 @@ import cartIcon6 from "@/assets/images/cleaningServiceImages/service-cat-icon-6.
 import cartIcon7 from "@/assets/images/cleaningServiceImages/service-cat-icon-7.png";
 import cartIcon8 from "@/assets/images/cleaningServiceImages/service-cat-icon-8.png";
 import cartIcon9 from "@/assets/images/cleaningServiceImages/service-cat-icon-9.png";
-import Image from "next/image";
 
 const TypesCleaning = () => {
   const { ref, inView } = useInView({ threshold: 0.1, triggerOnce: false });
 
   const CleaningVariants = {
-    hidden: { y: "8vh", opacity: 0 },
+    hidden: { y: "4vh", opacity: 0 },
     visible: {
       y: 0,
       opacity: 1,
@@ -29,7 +30,7 @@ const TypesCleaning = () => {
   };
 
   const CleaningHeadingVariants = {
-    hidden: { y: "-10vh", opacity: 0 },
+    hidden: { y: "-5vh", opacity: 0 },
     visible: {
       y: 0,
       opacity: 1,
@@ -38,63 +39,38 @@ const TypesCleaning = () => {
   };
 
   return (
-    <div className="bg-cloudblue pt-10 w-full mx-auto max-w-[1200px]">
-      <div className="mt-10 mx-5 md:mx-10">
-        <motion.div
-          className="flex justify-center pt-8"
-          ref={ref}
-          variants={CleaningHeadingVariants}
-          initial="hidden"
-          animate={inView ? "visible" : "hidden"}
-        >
-          <Heading heading="Types of Cleaning Service" font="48px" />
-        </motion.div>
-
-        {/* First Row with 5 Items */}
-        <motion.div
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-8 mt-10"
-          ref={ref}
-          variants={CleaningVariants}
-          initial="hidden"
-          animate={inView ? "visible" : "hidden"}
-        >
-          {TypeCleaningData.slice(0, 5).map((Item, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center gap-3 text-center"
-            >
-              <Image src={Item.icon} alt={Item.name} />
-              <span className="text-darkocean font-semibold text-lg sm:text-xl">
-                {Item.name}
-              </span>
-            </div>
-          ))}
-        </motion.div>
-
-        {/* Second Row with 4 Items Centered */}
-        <motion.div
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 md:px-24 gap-8 mt-10 "
-          ref={ref}
-          variants={CleaningVariants}
-          initial="hidden"
-          animate={inView ? "visible" : "hidden"}
-        >
-          {/* <div className="col-span-1 lg:col-span-2"></div>{" "} */}
-          {/* Empty space on the left */}
-          {TypeCleaningData.slice(5).map((Item, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center gap-3 text-center"
-            >
-              <Image src={Item.icon} alt={Item.name} />
-              <span className="text-darkocean font-semibold text-lg sm:text-xl">
-                {Item.name}
-              </span>
-            </div>
-          ))}
-          <div className="col-span-1 lg:col-span-2"></div>{" "}
-          {/* Empty space on the right */}
-        </motion.div>
+    <div className="w-full bg-cloudblue flex justify-center">
+      <div className=" py-10 w-full max-w-[1200px]">
+        <div className="">
+          <motion.div
+            className="flex text-cyanblue justify-center"
+            ref={ref}
+            variants={CleaningHeadingVariants}
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+          >
+            <Heading48 heading="Types of Cleaning Service" />
+          </motion.div>
+          <motion.div
+            className="flex w-full  justify-center flex-wrap mt-10"
+            ref={ref}
+            variants={CleaningVariants}
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+          >
+            {TypeCleaningData.map((Item, index) => (
+              <div
+                key={index}
+                className="flex min-w-[220px] mb-5 flex-col items-center gap-3 text-center"
+              >
+                <Image className="mt-5" height={100} width={100} src={Item.icon} alt={Item.name} />
+                <span className="text-darkocean font-semibold text-2xl">
+                  {Item.name}
+                </span>
+              </div>
+            ))}
+          </motion.div>
+        </div>
       </div>
     </div>
   );
